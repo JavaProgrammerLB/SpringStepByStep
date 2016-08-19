6b0b79cd81feb4c1d2491517d9f831d5d056bd52
LiuBei <liubeitongxue@icloud.com>
2016-08-19 10:22:22 +0800

    Java8 source code commit

---
+++ Java8/src/main/java/com/yitianyigexiangfa/java8/datetime/DateTimeGo.java
@@ -0,0 +1,13 @@
+package com.yitianyigexiangfa.java8.datetime;
+
+import java.time.LocalDate;
+
+public class DateTimeGo {
+
+	public static void main(String[] args) {
+		LocalDate date = LocalDate.of(2016, 05, 20);
+		System.out.println("Month:" + date.getMonth());
+		System.out.println("Isn't leap year:" + date.isLeapYear());
+	}
+
+}
