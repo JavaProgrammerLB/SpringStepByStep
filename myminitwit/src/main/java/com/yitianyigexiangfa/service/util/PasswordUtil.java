3dd65c58975ca46a5ec258ed5f38a47e14efb719
LiuBei <liubeitongxue@icloud.com>
2016-08-24 16:35:06 +0800

    Generate gravatar  and md5 password
    Util file

---
+++ myminitwit/src/main/java/com/yitianyigexiangfa/service/util/PasswordUtil.java
@@ -0,0 +1,16 @@
+package com.yitianyigexiangfa.service.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtil {
+	
+	public static String hashPassword(String pwd) {
+		String hashed = BCrypt.hashpw(pwd, BCrypt.gensalt());
+		return hashed;
+	}
+	
+	public static boolean verifyPassword(String pwd, String hash){
+		boolean b = BCrypt.checkpw(pwd, hash);
+		return b;
+	}
+}
