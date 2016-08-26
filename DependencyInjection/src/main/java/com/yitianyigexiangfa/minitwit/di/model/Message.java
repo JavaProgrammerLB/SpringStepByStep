c433c2c4c58650472aeb7154f07b53ced526bbfe
LiuBei <liubeitongxue@icloud.com>
2016-08-26 14:46:47 +0800

    DI in project myminitwit:Jave Bean

---
+++ DependencyInjection/src/main/java/com/yitianyigexiangfa/minitwit/di/model/Message.java
@@ -0,0 +1,33 @@
+package com.yitianyigexiangfa.minitwit.di.model;
+
+public class Message {
+	private int id;
+	private int userId;
+	private String username;
+	private String text;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+}
---
+++ DependencyInjection/src/main/java/com/yitianyigexiangfa/minitwit/di/model/User.java
@@ -0,0 +1,36 @@
+package com.yitianyigexiangfa.minitwit.di.model;
+
+public class User {
+
+	private int id;
+	private String username;
+	private String email;
+	private String password;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
