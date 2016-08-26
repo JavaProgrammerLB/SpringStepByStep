d6f6a89e1fe272bd05f273de414004eb60dfc53c
LiuBei <liubeitongxue@icloud.com>
2016-08-26 13:51:12 +0800

    Add @Repository annotation in UserDaoImpl
    Use @Repository annotation in DaoImpl file

--- myminitwit/src/main/java/com/yitianyigexiangfa/dao/impl/UserDaoImpl.java
+++ myminitwit/src/main/java/com/yitianyigexiangfa/dao/impl/UserDaoImpl.java
@@ -9,10 +9,12 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.yitianyigexiangfa.dao.UserDao;
 import com.yitianyigexiangfa.model.User;
 
+@Repository
 public class UserDaoImpl implements UserDao {
 	
 	private NamedParameterJdbcTemplate template;
