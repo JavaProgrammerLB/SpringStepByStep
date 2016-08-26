bcc5f69c2cd6af4c070910d50d368693683d74cd
LiuBei <liubeitongxue@icloud.com>
2016-08-26 13:49:01 +0800

    Enable WebConfig in App main function

--- myminitwit/src/main/java/com/yitianyigexiangfa/App.java
+++ myminitwit/src/main/java/com/yitianyigexiangfa/App.java
@@ -4,13 +4,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.yitianyigexiangfa.config.WebConfig;
+import com.yitianyigexiangfa.service.impl.MiniTwitService;
+
 @Configuration
 @ComponentScan({"com.yitianyigexiangfa"})
 public class App {
 	
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(App.class);
-//		new WebConfig(ctx.getBean(MiniTwitService.class));
+		new WebConfig(ctx.getBean(MiniTwitService.class));
 		ctx.registerShutdownHook();
 	}
 }
