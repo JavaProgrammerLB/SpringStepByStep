648d547410841be909f873b0142dfe2ebd0c4a96
LiuBei <liubeitongxue@icloud.com>
2016-08-26 14:47:46 +0800

    DI in project myminitwit:DataBase Config

---
+++ DependencyInjection/src/main/java/com/yitianyigexiangfa/minitwit/di/config/DatabaseConfig.java
@@ -0,0 +1,20 @@
+package com.yitianyigexiangfa.minitwit.di.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+public class DatabaseConfig {
+	
+	@Bean
+	public DataSource dataSource(){
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.HSQL).build();
+		return db;
+	}
+}
