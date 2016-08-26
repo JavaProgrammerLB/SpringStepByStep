03b7d03ff86bfe2c0e6c63e11a5859e4e58b9cb1
LiuBei <liubeitongxue@icloud.com>
2016-08-26 15:44:56 +0800

    create dataSource with mysql

--- myminitwit/src/main/java/com/yitianyigexiangfa/config/DatabaseConfig.java
+++ myminitwit/src/main/java/com/yitianyigexiangfa/config/DatabaseConfig.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -13,11 +14,11 @@ public class DatabaseConfig {
 	
 	@Bean
 	public DataSource dataSource(){
-		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.HSQL)
-		.addScript("sql/create-db.sql")
-		.addScript("sql/insert-data.sql")
-		.build();
-		return db;
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/myminitwit");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		return dataSource;
 	}
 }
