a9b892df66a64f916dccb81f10b0ee514ecb0b9b
LiuBei <liubeitongxue@icloud.com>
2016-09-12 18:28:35 +0800

    req.attribute req.attributes

---
+++ java-spark/src/main/java/com/yitianyigexiangfa/spark/FilterExampleAttributes.java
@@ -0,0 +1,32 @@
+package com.yitianyigexiangfa.spark;
+
+import static spark.Spark.*;
+
+public class FilterExampleAttributes {
+
+	public static void main(String[] args) {
+		
+		port(8076);
+		
+		get("/hi", (req,res) -> {
+			req.attribute("foo", "bar2");
+			return null;
+		});
+		
+		after("/hi", (req, res) -> {
+			for(String attr: req.attributes()){
+				System.out.println("attr: " + attr);
+			}
+		});
+		
+		after("/hi", (req, res) -> {
+			Object foo = req.attribute("foo");
+			res.body(asXml("foo", foo));
+		});
+		
+	}
+	private static String asXml(String name, Object value){
+		return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><" + name + ">" + value + "</" + name + ">";
+	}
+
+}
