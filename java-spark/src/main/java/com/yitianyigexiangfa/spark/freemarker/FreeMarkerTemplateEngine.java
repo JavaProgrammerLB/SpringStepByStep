5b76c42db94adfa77663061db7581e892685b91d
LiuBei <liubeitongxue@icloud.com>
2016-09-22 18:09:27 +0800

    Freemarker code example start

---
+++ java-spark/src/main/java/com/yitianyigexiangfa/spark/freemarker/FreeMarkerTemplateEngine.java
@@ -0,0 +1,15 @@
+package com.yitianyigexiangfa.spark.freemarker;
+
+import spark.ModelAndView;
+import spark.TemplateEngine;
+
+public class FreeMarkerTemplateEngine extends TemplateEngine {
+
+	@Override
+	public String render(ModelAndView modelAndView) {
+		return null;
+	}
+
+
+
+}
