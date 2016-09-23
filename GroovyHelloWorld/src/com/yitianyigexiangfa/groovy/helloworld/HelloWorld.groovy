5e6f8964b012e988ae79db9100126645cdefc98d
LiuBei <liubeitongxue@icloud.com>
2016-09-23 15:53:03 +0800

    Say Hello World with groovy

---
+++ GroovyHelloWorld/src/com/yitianyigexiangfa/groovy/helloworld/HelloWorld.groovy
@@ -0,0 +1,13 @@
+package com.yitianyigexiangfa.groovy.helloworld
+
+class HelloWorld {
+
+	public HelloWorld() {
+	}
+	
+	public static void main(def args){
+		println "Hello World"
+		println "Hi, Everyone"
+	}
+	
+}
