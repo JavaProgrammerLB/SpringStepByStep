530251c02495415bbfce687aadb712003504a782
LiuBei <liubeitongxue@icloud.com>
2016-09-29 17:07:32 +0800

    import static spark.Spark.*;
    STATIC

--- java-spark/src/main/java/com/yitianyigexiangfa/spark/JsonAcceptTypeExample.java
+++ java-spark/src/main/java/com/yitianyigexiangfa/spark/JsonAcceptTypeExample.java
@@ -1,13 +1,13 @@
 package com.yitianyigexiangfa.spark;
 
-import spark.Spark.*;
+import static spark.Spark.*;
 
 public class JsonAcceptTypeExample {
 
 	public static void main(String[] args) {
-//		get("/hello", "application/json", (req,res)->{
-//			return "{\"message\": \"Hello Workd\"}";
-//		});
+		get("/hello", "application/json", (req,res)->{
+			return "{\"message\": \"Hello World\"}";
+		});
 	}
 
 }
