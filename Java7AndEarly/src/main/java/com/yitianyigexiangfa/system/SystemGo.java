a6a592c12425ca6c014b8ee2d8d4dd8623dd4023
LiuBei <liubeitongxue@icloud.com>
2016-10-10 18:07:52 +0800

    System program with java

---
+++ Java7AndEarly/src/main/java/com/yitianyigexiangfa/system/SystemGo.java
@@ -0,0 +1,12 @@
+package com.yitianyigexiangfa.system;
+
+public class SystemGo {
+
+	public SystemGo() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("user.dir"));
+	}
+
+}
---
+++ Java7AndEarly/src/main/java/com/yitianyigexiangfa/system/cmd/CmdGo2.java
@@ -0,0 +1,29 @@
+package com.yitianyigexiangfa.system.cmd;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CmdGo2 {
+
+	public CmdGo2() {
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		try {
+			Process p = Runtime.getRuntime().exec("svn help");
+			BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String line = null;
+			System.out.println("hello");
+			while((line = in.readLine()) != null){
+				System.out.println(line);
+			}
+			int exitVal = p.waitFor();
+			System.out.println("Process exitValue：" + exitVal);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
