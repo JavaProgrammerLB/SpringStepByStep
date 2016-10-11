feed5f7fab1a0806c66e1cfa03b1841a27c50306
LiuBei <liubeitongxue@icloud.com>
2016-10-11 16:54:09 +0800

    java timer

---
+++ Java7AndEarly/src/main/java/com/yitianyigexiangfa/time/TimerTaskTest01.java
@@ -0,0 +1,13 @@
+package com.yitianyigexiangfa.time;
+
+import java.util.TimerTask;
+
+public class TimerTaskTest01 extends  TimerTask{
+
+	public TimerTaskTest01() {
+	}
+	
+	public void run(){
+		System.out.println("Time's up!!!");
+	}
+}
