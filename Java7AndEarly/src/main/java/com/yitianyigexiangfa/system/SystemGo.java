63c3c4696d3e0dfda3f5b94f0aea73a50d6f5a9a
LiuBei <liubeitongxue@icloud.com>
2016-10-21 18:16:02 +0800

    use System.getProperty

--- Java7AndEarly/src/main/java/com/yitianyigexiangfa/system/SystemGo.java
+++ Java7AndEarly/src/main/java/com/yitianyigexiangfa/system/SystemGo.java
@@ -6,7 +6,19 @@ public class SystemGo {
 	}
 
 	public static void main(String[] args) {
-		System.out.println(System.getProperty("user.dir"));
+		String cwd = System.getProperty("user.dir");
+		String javaRuntimeVersion = System.getProperty("java.runtime.version");
+		String jvmInfo = System.getProperty("java.vm.info");
+		String jvmVersion = System.getProperty("java.vm.version");
+		String jvmvendor = System.getProperty("java.vm.vendor");
+		String jvmName = System.getProperty("java.vm.name");
+		System.out.println(cwd);
+		System.out.println(javaRuntimeVersion);
+		System.out.println(jvmInfo);
+		System.out.println(jvmVersion);
+		System.out.println(jvmvendor);
+		System.out.println(jvmName);
+		
 	}
 
 }
