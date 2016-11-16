9a3f3d56efbed168913c64af54d833e1ca38e367
LiuBei <liubeitongxue@icloud.com>
2016-11-16 22:46:18 +0800

    send email with javax mail api
    run with java8

---
+++ java-mail-demo/src/main/java/com/yitianyigexiangfa/javax/mail/SendHTMLEmail.java
@@ -0,0 +1,69 @@
+package com.yitianyigexiangfa.javax.mail;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendHTMLEmail {
+   public static void main(String[] args) {
+      // Recipient's email ID needs to be mentioned.
+      String to = "destinationemail@gmail.com";
+
+      // Sender's email ID needs to be mentioned
+      String from = "fromemail@gmail.com";
+      final String username = "manishaspatil";//change accordingly
+      final String password = "******";//change accordingly
+
+      // Assuming you are sending email through relay.jangosmtp.net
+      String host = "relay.jangosmtp.net";
+
+      Properties props = new Properties();
+      props.put("mail.smtp.auth", "true");
+      props.put("mail.smtp.starttls.enable", "true");
+      props.put("mail.smtp.host", host);
+      props.put("mail.smtp.port", "25");
+
+      // Get the Session object.
+      Session session = Session.getInstance(props,
+         new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+               return new PasswordAuthentication(username, password);
+            }
+	});
+
+      try {
+            // Create a default MimeMessage object.
+            Message message = new MimeMessage(session);
+
+   	   // Set From: header field of the header.
+	   message.setFrom(new InternetAddress(from));
+
+	   // Set To: header field of the header.
+	   message.setRecipients(Message.RecipientType.TO,
+              InternetAddress.parse(to));
+
+	   // Set Subject: header field
+	   message.setSubject("Testing Subject");
+
+	   // Send the actual HTML message, as big as you like
+	   message.setContent(
+              "<h1>This is actual message embedded in HTML tags</h1>",
+             "text/html;charset=utf-8");
+
+	   // Send message
+	   Transport.send(message);
+
+	   System.out.println("Sent message successfully....");
+
+      } catch (MessagingException e) {
+	   e.printStackTrace();
+	   throw new RuntimeException(e);
+      }
+   }
+}
