e4d58444279e74dc8318804e36ce2fdb4a82d739
LiuBei <liubeitongxue@icloud.com>
2017-02-06 18:43:25 +0800

    IO

---
+++ Java7AndEarly/src/main/java/com/yitianyigexiangfa/io/JavaFile.java
@@ -0,0 +1,13 @@
+package com.yitianyigexiangfa.io;
+
+public class JavaFile {
+
+	public JavaFile() {
+	}
+
+	public static void main(String[] args) {
+		String s = System.getProperty("user.home");
+		System.getProperties();
+		System.out.println("--" + s);
+	}
+}
