9941a7687cd4aa435431419d0e16efc53441642e
刘备 <admin@yitianyigexiangfa.com>
2017-06-02 16:36:44 +0800

    2017年06月02日 spring mvc练功

---
+++ SpringMaster/2017_06_02/springmvc-20170602/src/main/java/com/yitianyigeixiang/springmvc/Application.java
@@ -0,0 +1,13 @@
+package com.yitianyigeixiang.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
