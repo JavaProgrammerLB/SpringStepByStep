06b845d3b0e5233215a20ffa75fb0ae579d80de9
刘备 <admin@yitianyigexiangfa.com>
2017-06-02 15:39:05 +0800

    2017年06月02日 mybatis练功，增删改查的基本实现

--- SpringMaster/2017_06_02/mybatisCRUD-20170602/src/main/java/com/yitianyigexiangfa/mybatis/mapper/UserMapper.java
+++ SpringMaster/2017_06_02/mybatisCRUD-20170602/src/main/java/com/yitianyigexiangfa/mybatis/mapper/UserMapper.java
@@ -8,9 +8,9 @@ public interface UserMapper {
 
 	public int insertUser(User user) throws Exception;
 	
-	public int deleteUser(User user) throws Exception;
+	public int deleteUser(int id) throws Exception;
 	
-	public User updateUser(User user, int id) throws Exception;
+	public int updateUser(User user) throws Exception;
 	
 	public User selectUserById(int id) throws Exception;
 	
