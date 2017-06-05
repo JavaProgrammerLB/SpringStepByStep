77dfea451ca9bdcd17430282221fd8bba99a0c33
刘备 <admin@yitianyigexiangfa.com>
2017-06-05 17:09:01 +0800

    2017年06月05日 spring MVC练功

---
+++ SpringMaster/2017_06_05/springMVC-20170605/src/main/java/com/yitianyigexiangfa/springmvc/GreetingController.java
@@ -0,0 +1,17 @@
+package com.yitianyigexiangfa.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+	
+	@RequestMapping(value="/greeting")
+	public String greeting(@RequestParam(value="name", required=false, defaultValue="world")String name, Model model){
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+	
+}
