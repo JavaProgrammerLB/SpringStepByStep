a8160ac2870183ab12fccb678fce340582b8a8ef
刘备 <admin@yitianyigexiangfa.com>
2017-06-05 15:23:42 +0800

    2017年06月05日 spring IoC练功

---
+++ SpringMaster/2017_06_05/springIoC-20170605/src/main/java/com/yitianyigexiangfa/spring/MessagePrinter.java
@@ -0,0 +1,19 @@
+package com.yitianyigexiangfa.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessagePrinter {
+	
+	private MessageService service;
+	
+	@Autowired
+	public MessagePrinter(MessageService service){
+		this.service = service;
+	}
+	
+	public void printMessage(){
+		System.out.println(this.service.getMessage());
+	}
+}
