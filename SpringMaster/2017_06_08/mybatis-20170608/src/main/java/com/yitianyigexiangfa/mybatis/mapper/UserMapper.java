62153bcf181845ed318225cf3537ba6bd4d11f28
刘备 <admin@yitianyigexiangfa.com>
2017-06-08 23:36:07 +0800

    2017年06月08日 mybatis练功

---
+++ SpringMaster/2017_06_08/mybatis-20170608/src/main/java/com/yitianyigexiangfa/mybatis/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.yitianyigexiangfa.mybatis.mapper;
+
+import java.util.List;
+
+import com.yitianyigexiangfa.mybatis.domain.User;
+
+public interface UserMapper {
+	
+	public int insertUser(User user) throws Exception;
+	
+	public int deleteUser(int id) throws Exception;
+	
+	public void updateUser(User user) throws Exception;
+	
+	public User selectUserById(int id) throws Exception;
+	
+	public List<User> selectAllUsers() throws Exception;
+
+}
