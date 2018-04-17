80665c92d3a8550f1faeccfc6abc99b9c8c231c1
刘备 <admin@yitianyigexiangfa.com>
2018-04-17 18:43:32 +0800

    spring-data操作redis

---
+++ redis-common/spring-data-quickstart/src/main/java/com/yitianyigexiangfa/data/redis/springdataquickstart/SpringDataQuickstartApplication.java
@@ -0,0 +1,12 @@
+package com.yitianyigexiangfa.data.redis.springdataquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataQuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataQuickstartApplication.class, args);
+	}
+}
